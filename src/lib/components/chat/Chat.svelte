38c825721b1d0728a7d18025b361045f66abdb64
Jiho YANG <jyangca@connect.ust.hk>
2025-02-22 17:28:39 +0800

    ADD: Sidebar에 current credit 표시되도록 수정

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -36,7 +36,8 @@
 		chatTitle,
 		showArtifacts,
 		tools,
-		toolServers
+		toolServers,
+		credit
 	} from '$lib/stores';
 	import {
 		convertMessagesToHistory,
@@ -68,7 +69,7 @@
 	import { processWeb, processWebSearch, processYoutubeVideo } from '$lib/apis/retrieval';
 	import { createOpenAITextStream } from '$lib/apis/streaming';
 	import { queryMemory } from '$lib/apis/memories';
-	import { getAndUpdateUserLocation, getUserSettings } from '$lib/apis/users';
+	import { getAndUpdateUserLocation, getUserCredit, getUserSettings } from '$lib/apis/users';
 	import {
 		chatCompleted,
 		generateQueries,
@@ -956,6 +957,10 @@
 		}, 1000);
 	};
 
+	const initCredit = async () => {
+		await credit.set(await getUserCredit(localStorage.token));
+	}
+
 	const createMessagePair = async (userPrompt) => {
 		prompt = '';
 		if (selectedModels.length === 0) {
@@ -1654,6 +1659,7 @@
 
 		await tick();
 		scrollToBottom();
+		initCredit();
 	};
 
 	const handleOpenAIError = async (error, responseMessage) => {
